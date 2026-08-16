30744a2b128447c92a8f6ea4b19e5728609973eb
vitoriapguimaraes <vpistoriguimaraes@gmail.com>
2026-08-16 09:37:37 -0300

    refactor/ assets naming

--- src/utils/config.py
+++ src/utils/config.py
@@ -8,21 +8,21 @@ DEMOS_DIR = ASSETS_DIR / "demos"
 
 # Dashboard Previews
 IMG_PREVIEW_CLASSIFICATION = str(
-    DEMOS_DIR / "Image_Classification_CIFAR10_display_preview.jpg"
+    DEMOS_DIR / "image_classification_cifar10_display_preview.jpg"
 )
-IMG_PREVIEW_TRAFFIC = str(DEMOS_DIR / "Vehicle_Counting_display_preview.jpg")
-IMG_PREVIEW_TRACKING = str(DEMOS_DIR / "Hand_Tracking_display_preview.jpg")
+IMG_PREVIEW_TRAFFIC = str(DEMOS_DIR / "vehicle_counting_display_preview.jpg")
+IMG_PREVIEW_TRACKING = str(DEMOS_DIR / "hand_tracking_display_preview.jpg")
 IMG_PREVIEW_DROWSINESS = str(
-    DEMOS_DIR / "Driver_Drowsiness_Detection_display_preview.jpg"
+    DEMOS_DIR / "driver_drowsiness_detection_display_preview.jpg"
 )
 
 # Demos (GIFs)
-DEMO_CLASSIFICATION = str(DEMOS_DIR / "Image_Classification_CIFAR10_display_opt.gif")
-DEMO_TRAFFIC = str(DEMOS_DIR / "Vehicle_Counting_display_opt.gif")
-DEMO_TRACKING = str(DEMOS_DIR / "Hand_Tracking_display_opt.gif")
+DEMO_CLASSIFICATION = str(DEMOS_DIR / "image_classification_cifar10_display_opt.gif")
+DEMO_TRAFFIC = str(DEMOS_DIR / "vehicle_counting_display_opt.gif")
+DEMO_TRACKING = str(DEMOS_DIR / "hand_tracking_display_opt.gif")
 DEMO_LANE_DETECTION = str(DEMOS_DIR / "highway_lane_detection_opt.gif")
-DEMO_DROWSINESS = str(DEMOS_DIR / "Driver_Drowsiness_Detection_display_opt.gif")
-DEMO_LICENSE_PLATE = str(DEMOS_DIR / "demo_license_plate.jpg")
+DEMO_DROWSINESS = str(DEMOS_DIR / "driver_drowsiness_detection_display_opt.gif")
+DEMO_LICENSE_PLATE = str(DEMOS_DIR / "reading_licence_plate_creative.jpg")
 
 # Image Classification specific
 IMG_CIFAR10_CLASSES = str(ASSETS_DIR / "cifar_images" / "cifar10_classes_1_line.jpg")
